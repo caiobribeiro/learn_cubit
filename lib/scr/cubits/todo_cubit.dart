b870a89501c081a8c55b1b7f47a9becbe0da4350
Caio Beraldi Ribeiro <44821827+caiobribeiro@users.noreply.github.com>
2024-03-05 18:30:17 -0300

    Homepage widget update

--- lib/scr/cubits/todo_cubit.dart
+++ lib/scr/cubits/todo_cubit.dart
@@ -4,17 +4,17 @@ import 'package:learn_cubit/scr/cubits/todo_states.dart';
 class TodoCubit extends Cubit<TodoStates> {
   final List<String> _todos = [];
 
-  List<String> get todo => _todos;
+  List<String> get todos => _todos;
 
   TodoCubit() : super(InitialTodoState());
 
   Future<void> addTodo({required String todo}) async {
     emit(LoadingTodoState());
 
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: 100));
 
     if (_todos.contains(todo)) {
-      emit(ErrorTodoState(message: 'Message already on the list'));
+      emit(ErrorTodoState(message: 'Todo already on the list'));
     } else {
       _todos.add(todo);
       emit(DoneTodoState(todos: _todos));
@@ -24,7 +24,7 @@ class TodoCubit extends Cubit<TodoStates> {
   removeTodo({required int index}) async {
     emit(LoadingTodoState());
 
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: 100));
 
     _todos.removeAt(index);
 
--- lib/scr/home_page.dart
+++ lib/scr/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learn_cubit/scr/cubits/todo_cubit.dart';
+import 'package:learn_cubit/scr/cubits/todo_states.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late final TodoCubit cubit;
-  TextEditingController textEditingController = TextEditingController();
+  final TextEditingController _textEditingController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     cubit = BlocProvider.of<TodoCubit>(context);
+    cubit.stream.listen((state) {
+      if (state is ErrorTodoState) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(state.message),
+          ),
+        );
+      }
+    });
   }
 
   @override
@@ -25,41 +35,83 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Todo with Cubit'),
       ),
-      body: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    offset: const Offset(0, -5),
-                    blurRadius: 4,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                BlocBuilder(
+                  bloc: cubit,
+                  builder: (context, state) {
+                    if (state is InitialTodoState) {
+                      return const Text('Empty list');
+                    } else if (state is LoadingTodoState) {
+                      return const CircularProgressIndicator();
+                    } else if (state is DoneTodoState) {
+                      return _buildTodoList(state.todos);
+                    } else {
+                      return _buildTodoList(cubit.todos);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  offset: const Offset(0, -5),
+                  blurRadius: 4,
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(16),
+            child: SafeArea(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _textEditingController,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      cubit.addTodo(todo: _textEditingController.text);
+                      _textEditingController.clear();
+                    },
+                    icon: const Icon(Icons.add),
                   ),
                 ],
-              ),
-              child: SafeArea(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        controller: textEditingController,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: textEditingController.clear,
-                      icon: const Icon(Icons.add),
-                    ),
-                  ],
-                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTodoList(List<String> todos) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: todos.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: CircleAvatar(
+          child: Text(todos[index][0]),
+        ),
+        title: Text(
+          todos[index],
+        ),
+        trailing: IconButton(
+          onPressed: () {
+            cubit.removeTodo(index: index);
+          },
+          icon: const Icon(Icons.delete),
+        ),
       ),
     );
   }
